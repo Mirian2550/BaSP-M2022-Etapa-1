f03082f89ae5d65e4826feedc42d34b3ba3a9027
Mirian2550 <miri2550@gmail.com>
2022-04-19 02:24:39 -0300

    BaSP-W05-2022: finish exercise 6

--- semana-05/exercises/06_functions.js
+++ semana-05/exercises/06_functions.js
@@ -7,8 +7,8 @@ la consola del navegador.
 console.log('Exercise 6-a');
 
 function suma(x1, x2) {
-    var result = x1 + x2;
-    console.log('The result of the sum is: ' + result);
+	var result = x1 + x2;
+	console.log('The result of the sum is: ' + result);
 }
 suma(8, 'dog')
 
@@ -21,16 +21,16 @@ tiene error y retornar el valor NaN como resultado.
 console.log('Exercise 6-b');
 
 function validationSuma(x1, x2) {
-    if (typeof(x1) != "number" || typeof(x2) != "number") {
-        alert('Parameter error');
-        return NaN;
-    } else {
-        return console.log(x1 + x2);
-    }
+	if (typeof(x1) != "number" || typeof(x2) != "number") {
+		alert('Parameter error');
+		return NaN;
+	} else {
+		return console.log(x1 + x2);
+	}
 }
 validationSuma(8, 'dog');
 validationSuma(4, 2); //undefined
-validationSuma('m', '3'); 
+validationSuma('m', '3');
 validationSuma('+', 9);
 validationSuma('6', 1);
 validationSuma(6, 1);
@@ -40,8 +40,9 @@ c. Crear una función validate integer que reciba un número como parámetro y d
 verdadero si es un número entero.
 */
 console.log('Exercise 6-c');
+
 function validateInteger(number) {
-    return Number.isInteger(number)
+	return Number.isInteger(number)
 }
 
 console.log(validateInteger(8));
@@ -54,18 +55,18 @@ número convertido a entero (redondeado).
 console.log('Exercise 6-d');
 
 function Suma(x1, x2) {
-    if (typeof(x1) !== "number" || typeof(x2) !== "number") {
-        alert('Parameter error');
-        return NaN;
-    } 
-    if (validateInteger(x1)== false) {
-        alert('Parameter error 1' + Math.round(x1));
-    }
-    if (validateInteger(x1)== false) {
-        alert('Parameter error 2 ' + Math.round(x2))
-    } else {
-        return console.log(x1 + x2);
-    }
+	if (typeof(x1) !== "number" || typeof(x2) !== "number") {
+		alert('Parameter error');
+		return NaN;
+	}
+	if (validateInteger(x1) == false) {
+		alert('Parameter error 1' + Math.round(x1));
+	}
+	if (validateInteger(x1) == false) {
+		alert('Parameter error 2 ' + Math.round(x2))
+	} else {
+		return console.log(x1 + x2);
+	}
 }
 
 console.log(Suma(5.5, 5));
@@ -77,19 +78,20 @@ función suma probando que todo siga funcionando igual
 console.log('Exercise 6-e');
 
 function Suma(x1, x2) {
-    if (typeof(x1) !== "number" || typeof(x2) !== "number") {
-        alert('Parameter error');
-        return NaN;
-    } else {
-        return validatenext(x1,x2)
-    }
-} 
-function validatenext(x1,x2) { 
-    if (validateInteger(x1) && validateInteger(x2)) {
-        return x1+x2;
-    } else {
-        alert('Parameter error x ')
-        return Math.round(x1 + x2);
-    }        
+	if (typeof(x1) !== "number" || typeof(x2) !== "number") {
+		alert('Parameter error');
+		return NaN;
+	} else {
+		return validatenext(x1, x2)
+	}
+}
+
+function validatenext(x1, x2) {
+	if (validateInteger(x1) && validateInteger(x2)) {
+		return x1 + x2;
+	} else {
+		alert('Parameter error x ')
+		return Math.round(x1 + x2);
+	}
 }
 console.log(Suma(6, 3));
